83f24bf16ed15de84f1df63274ce86f59cbe4335
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-23 13:02:03 +0000

    Fix missing package

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,16 +1,20 @@
 <script setup>
-import TodoCreator from "../components/TodoCreator.vue";
-import { uid } from 'uid';
-const todoList = ref([]);
-
-const createTodo = (todo) => {
-  todoList.value.push({
-    id: uid(),
-    todo,
-    isCompleted: false,
-    isEditing: false
-  })
-}
+  import {
+    ref
+  } from "vue";
+  import TodoCreator from "../components/TodoCreator.vue";
+  import {
+    uid
+  } from 'uid';
+  const todoList = ref([]);
+  const createTodo = (todo) => {
+    todoList.value.push({
+      id: uid(),
+      todo,
+      isCompleted: false,
+      isEditing: false
+    })
+  }
 </script>
 
 <template>
