c56a426eda0fb10d914d15fcf1b4cef5399e278c
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-23 12:58:32 +0000

    Consume Emit on parent

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,11 +1,22 @@
 <script setup>
 import TodoCreator from "../components/TodoCreator.vue";
+import { uid } from 'uid';
+const todoList = ref([]);
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: false,
+    isEditing: false
+  })
+}
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
-    <todoCreator />
+    <TodoCreator @create-todo="createTodo" />
   </main>
 </template>
 
