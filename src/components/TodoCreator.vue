9c5273336dc76a8af72bcbcf50ab0cd38debd962
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-23 17:34:01 +0000

    Add event

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -24,8 +24,10 @@
     <form class="w-full max-w-lg">
         <div class="flex items-center border-b border-teal-500 py-2">
             <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Todo Title" 
-            aria-label="Todo Title"  v-model="todoState.todo" />
-            <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+            aria-label="Todo Title" v-model="todoState.todo" />
+            <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" 
+            type="button"
+            @click="createTodo()">
                 Create
             </button>
         </div>
