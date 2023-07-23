fa7a39251cfbdf96513702fc8a08b17ec7176cbd
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-23 11:51:11 +0000

    Vue Reactive input

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,46 @@
+<script setup>
+    import {
+        ref
+    } from "vue";
+    const todo = ref("Testing");
+</script>
+
+<template>
+    <div class="input-wrap">
+        <input type="text" v-model="todo" />
+        <button>Create</button>
+        <p>{{ todo }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .input-wrap {
+        display: flex;
+        transition: 250ms ease;
+        border: 2px solid #41b080;
+        &.input-err {
+            border-color: red;
+        }
+        &:focus-within {
+            box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1), 0 -2px 4px -2px rgb(0 0 0 / 0.1);
+        }
+        input {
+            width: 100%;
+            padding: 8px 6px;
+            border: none;
+            &:focus {
+                outline: none;
+            }
+        }
+        button {
+            padding: 8px 16px;
+            border: none;
+        }
+    }
+    .err-msg {
+        margin-top: 6px;
+        font-size: 12px;
+        text-align: center;
+        color: red;
+    }
+</style>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TodoCreator from "../components/TodoCreator.vue";
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <todoCreator />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    max-width: 500px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 40px 16px;
+    h1 {
+      margin-bottom: 16px;
+      text-align: center;
+    }
+  }
+</style>
